16b45bf9495e8800b7eeab3216b08f0244863c23
Eero Lehtonen <eeroalvar.lehtonen@gmail.com>
2021-06-21 19:58:18 +0300

    Created new function for getting the locations and implemented cors policies

--- cyclist-backend/app.py
+++ cyclist-backend/app.py
@@ -1,23 +1,39 @@
-
-from flask import Flask
+from flask import Flask, jsonify
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import json
 
 app = Flask(__name__)
 
-@app.route('/locations/<name>')
-def get_location_data(name):
-	conn_string = " port="+ "5432" +" dbname="+ "zadaa_exercice"
-	conn=psycopg2.connect(conn_string)
-	cur = conn.cursor(cursor_factory=RealDictCursor)
-	query = """
-	    SELECT cyclists.päivämäärä, cyclists.aika, sijainti, pyöräilijöitä, sademäärä, pilvien_määrä, suhteellinen_kosteus, sateen_intensiteetti, ilman_lämpötila, tuulen_nopeus
+@app.route('/locations/<id>')
+def get_location_data(id):
+  conn_string = " port=" + "5432" + " dbname=" + "zadaa_exercice"
+  conn = psycopg2.connect(conn_string)
+  cur = conn.cursor(cursor_factory=RealDictCursor)
+  query = f"""
+      SELECT cyclists.päivämäärä, cyclists.aika, sijainti, pyöräilijöitä, sademäärä, pilvien_määrä, suhteellinen_kosteus, sateen_intensiteetti, ilman_lämpötila, tuulen_nopeus
        		FROM weather, cyclists
-        	WHERE weather.päivämäärä = cyclists.päivämäärä AND weather.aika = cyclists.aika AND cyclists.sijainti = 'Auroransilta'
-	"""
-	cur.execute(query)
-	result = cur.fetchall()
-	conn.close()
-	return json.dumps(result)
+        	WHERE weather.päivämäärä = cyclists.päivämäärä AND weather.aika = cyclists.aika AND cyclists.sijainti = {id}
+  """
+  cur.execute(query)
+  result = cur.fetchall()
+  response = jsonify(data=result)
+  response.headers.add("Access-Control-Allow-Origin", "*")
+  conn.close()
+  return response
+
+@app.route('/locations/')
+def get_locations():
+  conn_string = " port=" + "5432" + " dbname=" + "zadaa_exercice"
+  conn = psycopg2.connect(conn_string)
+  cur = conn.cursor(cursor_factory=RealDictCursor)
+  query = """
+      SELECT * 
+        FROM locations
+  """
+  cur.execute(query)
+  result = cur.fetchall()
+  response = jsonify(data=result)
+  response.headers.add("Access-Control-Allow-Origin", "*")
+  conn.close()
+  return response
 	
