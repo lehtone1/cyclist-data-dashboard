0caa704ba44c93ed17841066f82ca9391ed51ef1
Eero Lehtonen <eeroalvar.lehtonen@gmail.com>
2021-06-22 10:29:52 +0300

    Small style fixes

--- cyclist-frontend/src/components/common/customScatterChart/CustomScatterChart.jsx
+++ cyclist-frontend/src/components/common/customScatterChart/CustomScatterChart.jsx
@@ -12,16 +12,18 @@ import {
 const CustomScatterChart = ({ data, heading }) => {
   return (
     <div className="flex flex-jc-c sm-ver-margin">
-      <p className="text-center">{heading}</p>
-      <ScatterChart width={730} height={250}
-        margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="ilman_lämpötila" name="Ilman lämpötila" unit="C" type="number"/>
-          <YAxis dataKey="pyöräilijöitä" name="Pyöräilijöitä" unit="kpl" />
-          <ZAxis dataKey="päivämäärä" name="Päivämäärä" />
-          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-          <Scatter name="Mittaukset" data={data} fill="#8884d8" />
-      </ScatterChart>
+      <div>
+        <p className="text-center">{heading}</p>
+        <ScatterChart width={730} height={250}
+          margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="ilman_lämpötila" name="Ilman lämpötila" unit="C" type="number"/>
+            <YAxis dataKey="pyöräilijöitä" name="Pyöräilijöitä" unit="kpl" />
+            <ZAxis dataKey="päivämäärä" name="Päivämäärä" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter name="Mittaukset" data={data} fill="#8884d8" />
+        </ScatterChart>
+      </div>
     </div>
   );
 };
